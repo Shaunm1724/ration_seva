efa6c163809b1b716158fbc293a11b80b338c419
ruchir404 <ruchirchitre@gmail.com>
2024-04-15 22:57:19 +0530

    added Map in discover page created 2 new pages- locations and shops

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:ration_seva/components/navbar.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'shops.dart';
 
 class Discover extends StatelessWidget {
-  const Discover({super.key});
+  const Discover({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +17,93 @@ class Discover extends StatelessWidget {
       backgroundColor: Color(0xffF4F3F9),
       bottomNavigationBar: NavBar(
         currentPageIndex: 1,
+      ),
+
+      body: MapApp(),
+      // Added the closing square bracket here
+    );
+  }
+}
+
+class MapApp extends StatefulWidget {
+  const MapApp({Key? key});
+
+  @override
+  State<MapApp> createState() => _MapAppState();
+}
+
+class _MapAppState extends State<MapApp> {
+  List<Marker> getMarkers() {
+    return List<Marker>.from(Shops.locations.map(
+      (e) => new Marker(
+          width: 150.0,
+          height: 150.0,
+          point: LatLng(e.latitude, e.longitude),
+          child: Icon(Icons.location_on, color: Colors.red, size: 40)),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: LatLng(19.08192, 72.8867),
+              initialZoom: 15,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(
+                markers: getMarkers(),
+              ),
+              RichAttributionWidget(
+                attributions: [
+                  TextSourceAttribution(
+                    'OpenStreetMap contributors',
+                    onTap: () => launchUrl(
+                      Uri.parse('https://openstreetmap.org/copyright'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+            left: 20,
+            right: 20,
+            top: 20,
+            child: Container(
+              height: 70,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xffFFFFFF),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 4),
+                    blurRadius: 4,
+                    color: Color(0xffbdbdc1),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Discover Ration centres',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/pages/location.dart
@@ -0,0 +1,6 @@
+class Location {
+  String name;
+  double latitude;
+  double longitude;
+  Location(this.name, this.latitude, this.longitude);
+}
