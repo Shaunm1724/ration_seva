8ae2791be6011ffc6d16295584697e6d913e640c
Shaunm1724 <shaun.menezes1724@gmail.com>
2024-04-25 16:02:42 +0530

    Path to navbar

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:ration_seva/components/navbar.dart';
+import 'package:ration_seva/components/Common/navbar.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'shops.dart';
